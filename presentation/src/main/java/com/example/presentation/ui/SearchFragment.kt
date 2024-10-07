af52ddf6f319da78714037c905bc2133067deb59
Lsunae <sjsao5012@gmail.com>
2024-10-07 23:39:44 +0900

    Feature: 검색/좋아요 BottomNavigation, Fragment 생성

---
+++ presentation/src/main/java/com/example/presentation/ui/SearchFragment.kt
@@ -0,0 +1,36 @@
+package com.example.presentation.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.presentation.R
+import com.example.presentation.databinding.FragmentSearchBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SearchFragment : Fragment() {
+    private lateinit var binding: FragmentSearchBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        binding.apply {
+            includeActionbar.tvTitle.text = getString(R.string.search)
+        }
+    }
+}
