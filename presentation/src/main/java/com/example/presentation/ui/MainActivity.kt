27c8b6973ea8a585aafc2930bc7eb4495c0b6a94
Lsunae <sjsao5012@gmail.com>
2024-10-07 21:48:02 +0900

    Feature: MainActivity 클래스 ui 폴더로 이동

--- presentation/src/main/java/com/example/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/example/presentation/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.presentation
+package com.example.presentation.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.presentation.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
