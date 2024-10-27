9f832fb72f9cc1c296fbe6a74f47bdc7d3366211
Lsunae <sjsao5012@gmail.com>
2024-10-27 21:30:14 +0900

    Feat: 좋아요 RecyclerView Adapter 셋팅

--- presentation/src/main/java/com/example/presentation/ui/LikeFragment.kt
+++ presentation/src/main/java/com/example/presentation/ui/LikeFragment.kt
@@ -5,13 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.presentation.R
 import com.example.presentation.databinding.FragmentLikeBinding
+import com.example.presentation.ui.adapter.BookRvAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LikeFragment : Fragment() {
     private lateinit var binding: FragmentLikeBinding
+    private lateinit var bookAdapter: BookRvAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,11 +29,20 @@ class LikeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setupView()
+        setAdapter()
     }
 
     private fun setupView() {
         binding.apply {
             incTitle.tvTitle.text = getString(R.string.like)
+        }
+    }
+
+    private fun setAdapter() {
+        bookAdapter = BookRvAdapter()
+        binding.rvBook.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = bookAdapter
         }
     }
 }
