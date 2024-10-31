b92e120cf53bb60edb70ed97d8470589816987df
Lsunae <sjsao5012@gmail.com>
2024-10-31 22:16:43 +0900

    Feat: Datasource 모듈에 LikeLocalDataSource 추가

--- app/src/main/java/com/example/clean_architecture_study/di/DataSourceModule.kt
+++ app/src/main/java/com/example/clean_architecture_study/di/DataSourceModule.kt
@@ -1,11 +1,15 @@
 package com.example.clean_architecture_study.di
 
+import android.content.Context
 import com.example.data.api.BookService
+import com.example.data.source.local.LikeLocalDataSource
+import com.example.data.source.local.LikeLocalDataSourceImpl
 import com.example.data.source.remote.BookRemoteDataSource
 import com.example.data.source.remote.BookRemoteDataSourceImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -18,5 +22,12 @@ class DataSourceModule {
         bookService: BookService
     ): BookRemoteDataSource {
         return BookRemoteDataSourceImpl(bookService)
+    }
+    @Provides
+    @Singleton
+    fun provideLikeDataSource(
+        @ApplicationContext context: Context
+    ): LikeLocalDataSource {
+        return LikeLocalDataSourceImpl(context)
     }
 }
