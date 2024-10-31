e74af5d3fa08b9b5fb2be39f912dfd2a9ddf3e12
Lsunae <sjsao5012@gmail.com>
2024-10-31 22:20:46 +0900

    Refactor: 좋아요 데이터 모델 변경 적용 및 DataSource 함수 적용

--- data/src/main/java/com/example/data/repository/LikeRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/LikeRepositoryImpl.kt
@@ -1,21 +1,19 @@
 package com.example.data.repository
 
 import com.example.data.source.local.LikeLocalDataSource
-import com.example.domain.model.LikeModel
+import com.example.domain.model.Book
 import com.example.domain.repository.LikeRepository
 import javax.inject.Inject
 
 class LikeRepositoryImpl @Inject constructor(private val dataSource: LikeLocalDataSource) :
     LikeRepository {
-    override suspend fun getLikeList(): MutableList<LikeModel> {
-        TODO("Not yet implemented")
+    override suspend fun getLikeList(): MutableList<Book> = dataSource.getLikeList()
+
+    override suspend fun addLike(item: Book) {
+        dataSource.addLike(item)
     }
 
-    override suspend fun addLike(like: LikeModel) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun deleteLike(like: LikeModel) {
-        TODO("Not yet implemented")
+    override suspend fun deleteLike(item: Book) {
+        dataSource.deleteLike(item)
     }
 }
