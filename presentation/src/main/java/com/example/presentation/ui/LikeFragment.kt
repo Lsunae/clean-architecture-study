987610da3974dacbd32a7ca1a7ba9880aa910261
Lsunae <sjsao5012@gmail.com>
2024-11-03 17:34:29 +0900

    Feat: 좋아요 화면 내 클릭 리스너 셋팅 코드 추가

--- presentation/src/main/java/com/example/presentation/ui/LikeFragment.kt
+++ presentation/src/main/java/com/example/presentation/ui/LikeFragment.kt
@@ -8,9 +8,11 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.domain.model.Book
 import com.example.presentation.R
 import com.example.presentation.databinding.FragmentLikeBinding
 import com.example.presentation.ui.adapter.BookRvAdapter
+import com.example.presentation.util.ClickListener
 import com.example.presentation.util.TabType
 import com.example.presentation.viewmodel.LikeViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -35,6 +37,7 @@ class LikeFragment : Fragment() {
 
         setupView()
         setAdapter()
+        setClickListener()
 
         getLikeList()
         likeViewModel.getLikeList()
@@ -52,6 +55,14 @@ class LikeFragment : Fragment() {
             layoutManager = LinearLayoutManager(requireContext())
             adapter = bookAdapter
         }
+    }
+
+    private fun setClickListener() {
+        bookAdapter.setOnLikeClickListener(object : ClickListener {
+            override fun onLikeClick(item: Book, isSelected: Boolean) {
+                likeViewModel.setLike(item, isSelected)
+            }
+        })
     }
 
     private fun getLikeList() {
