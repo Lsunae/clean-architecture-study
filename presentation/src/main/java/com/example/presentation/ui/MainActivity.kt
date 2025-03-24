962ddf62866515e65493d95e45accacb8ad67b5d
Lsunae <sjsao5012@gmail.com>
2025-03-24 12:23:41 +0900

    BottomNavigationView 미노출 이슈 수정

--- presentation/src/main/java/com/example/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/example/presentation/ui/MainActivity.kt
@@ -9,6 +9,7 @@ import com.example.presentation.R
 import com.example.presentation.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
